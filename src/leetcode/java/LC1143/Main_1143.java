e9ee50e85a114f6bc469a5de419df124a2e45006
dwj <dwj2930@163.com>
2020-05-02 00:16:20 +0800

    2020.05.01: (LC278), LC1143

---
+++ src/leetcode/java/LC1143/Main_1143.java
@@ -0,0 +1,56 @@
+package leetcode.java.LC1143;
+
+public class Main_1143 {
+
+    public static void main(String[] args) {
+        Main_1143 main_1143 = new Main_1143();
+        System.out.println(main_1143.longestCommonSubsequence_solution1("abee", "babcee"));
+        System.out.println(main_1143.longestCommonSubsequence_solution2("abee", "babcee"));
+    }
+
+
+    // Solution: Dynamic Programming
+    // Time: O(M*N)
+    // Space: O(M*N)
+    // Ref: https://leetcode-cn.com/problems/longest-common-subsequence/solution/dong-tai-gui-hua-zhi-zui-chang-gong-gong-zi-xu-lie/
+    public int longestCommonSubsequence_solution1(String text1, String text2) {
+        char[] t1 = text1.toCharArray();
+        char[] t2 = text2.toCharArray();
+        int length1 = t1.length, length2 = t2.length;
+        int[][] dp = new int[length1 + 1][length2 + 1];
+        for (int i = 1; i < length1 + 1; i++) {
+            for (int j = 1; j < length2 + 1; j++) {
+                if (t1[i - 1] == t2[j - 1]) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[length1][length2];
+    }
+
+
+    // Solution: Dynamic Programming  (Space Optimized)  2D dp array --> 1D dp array
+    // Time: O(M*N)
+    // Space: O(N)
+    public int longestCommonSubsequence_solution2(String text1, String text2) {
+        char[] t1 = text1.toCharArray();
+        char[] t2 = text2.toCharArray();
+        int[] dp = new int[t1.length + 1];
+        int ans = 0;
+        for(int j = 1; j <= t2.length; j++)
+        {
+            int last = 0;
+            for(int i = 1; i <= t1.length; i++)
+            {
+                int temp = dp[i];
+                dp[i] = t1[i-1] == t2[j-1] ? (last + 1) : Math.max(dp[i-1], dp[i]);
+                ans = Math.max(ans, dp[i]);
+                last = temp;
+            }
+        }
+        return ans;
+    }
+
+}
--- src/leetcode/java/LC278/Main_278.java
+++ src/leetcode/java/LC278/Main_278.java
@@ -5,14 +5,17 @@ public class Main_278 {
         Main_278 main_278 = new Main_278();
     }
 
-    // Solution: Binary Search
+    // Ref: https://leetcode.wang/leetcode-278-First-Bad-Version.html
+
+
+    // Solution1: Binary Search
     // Time: O(logN)  Space: O(1)
-    public int firstBadVersion(int n) {
+    public int firstBadVersion_solution1(int n) {
         int left = 1, right = n;
-        while (left < right) {
+        while (left < right) {      // Note: Key point here. When left == right == mid, the loop exits.
 
             // Note: Test Case: (n=2126753390 & firstBadVersion=1702766719)
-            // Mid的计算：(left + right) / 2 会导致溢出，必须使用 left + (right - left) / 2防止溢出！！
+            // Mid的计算：(left + right) / 2 会导致溢出，必须使用 left + (right - left) / 2防止溢出！！  或者使用：int mid = (left + right) >>> 1;
             int mid = left + (right - left) / 2;
 
             if (isBadVersion(mid)) {
@@ -25,10 +28,33 @@ public class Main_278 {
         return left;
     }
 
-    private boolean isBadVersion(int i) {
 
-        // To be implemented...
+    // Solution2: Naive Binary Search
+    // Time: O(logN)  Space: O(1)
+    public int firstBadVersion_solution2(int n) {
+        int low = 1;
+        int high = n;
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            if (isBadVersion(mid)) {
+                if (mid == 1) {     // check if `mid` is the 1st version. (The version number ranges from 1 to N, not 0 to N !)
+                    return 1;
+                }
+                //判断前一个是否是 false
+                if (!isBadVersion(mid - 1)) {
+                    return mid;
+                }
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
 
-        return false;
+    private boolean isBadVersion(int n) {
+        // Implemented by leetcode.com
+        return true;
+        // return false;
     }
 }
